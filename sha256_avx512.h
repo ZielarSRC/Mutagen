c7531416be7700d0b95fb2d8e66f7eb8faf4d474
Aleksander <poczta@zielar.pl>
2025-05-23 00:23:14 +0200

    Update sha256_avx512.h

--- sha256_avx512.h
+++ sha256_avx512.h
@@ -8,8 +8,20 @@
 extern "C" {
 #endif
 
-// Process 32 blocks in parallel
-void sha256_avx512_32blocks(const uint8_t* data[32], uint8_t* out_hashes[32]);
+// Maksymalna wydajność AVX-512: batch 32 bloków (64B na blok, 32x32B hash)
+void sha256_avx512_32blocks(const uint8_t* in[32], uint8_t* out[32]);
+
+// Batch 16x (wywołuje batch 32x, pozostałe sloty zeruje)
+void sha256_avx512_16blocks(const uint8_t* in[16], uint8_t* out[16]);
+
+// Batch 8x (wywołuje batch 32x, pozostałe sloty zeruje)
+void sha256_avx512_8blocks(const uint8_t* in[8], uint8_t* out[8]);
+
+// Batch 4x (wywołuje batch 32x, pozostałe sloty zeruje)
+void sha256_avx512_4blocks(const uint8_t* in[4], uint8_t* out[4]);
+
+// Batch 1x (wywołuje batch 32x, pozostałe sloty zeruje)
+void sha256_avx512_1block(const uint8_t* in, uint8_t* out);
 
 #ifdef __cplusplus
 }
