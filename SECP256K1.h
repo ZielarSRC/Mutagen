ba105e9cd2d6e6fe0484510d1c6f17d321e04bf8
Aleksander <poczta@zielar.pl>
2025-05-23 00:24:25 +0200

    Update SECP256K1.h

--- SECP256K1.h
+++ SECP256K1.h
@@ -2,13 +2,16 @@
 #define SECP256K1H
 
 #include "Point.h"
+#include <string>
 #include <vector>
+#include <cstdint>
 
 #define P2PKH  0
 #define P2SH   1
 #define BECH32 2
 
 class Secp256K1 {
+
 public:
     Secp256K1();
     ~Secp256K1();
@@ -20,26 +23,41 @@ public:
     bool EC(Point &p);
     Int GetY(Int x, bool isEven);
 
-    // AVX-512 accelerated methods
-    __attribute__((target("avx512f"))) Point Add(Point &p1, Point &p2);
-    __attribute__((target("avx512f"))) Point Add2(Point &p1, Point &p2);
-    __attribute__((target("avx512f"))) Point AddDirect(Point &p1, Point &p2);
-    __attribute__((target("avx512f"))) Point Double(Point &p);
-    __attribute__((target("avx512f"))) Point DoubleDirect(Point &p);
-
-    // Utility methods
+    // AVX-512 batch-accelerated hash160 (16x)
+    void GetHash160_Batch16(int type, bool compressed,
+        Point* pubkeys[16], uint8_t* hashes[16]);
+    void GetHash160_Batch8(int type, bool compressed,
+        Point* pubkeys[8], uint8_t* hashes[8]);
+    void GetHash160_Batch4(int type, bool compressed,
+        Point* pubkeys[4], uint8_t* hashes[4]);
     void GetHash160(int type, bool compressed, Point &pubKey, unsigned char *hash);
+
     std::string GetAddress(int type, bool compressed, Point &pubKey);
+    std::string GetAddress(int type, bool compressed, unsigned char *hash160);
+    std::vector<std::string> GetAddress(int type, bool compressed, unsigned char *h1, unsigned char *h2, unsigned char *h3, unsigned char *h4);
     std::string GetPrivAddress(bool compressed, Int &privKey);
+    std::string GetPublicKeyHex(bool compressed, Point &p);
+    Point ParsePublicKeyHex(std::string str, bool &isCompressed);
+
+    bool CheckPudAddress(std::string address);
+
     static Int DecodePrivateKey(char *key, bool *compressed);
 
-    // Generator table with 64-byte alignment for AVX-512
+    Point Add(Point &p1, Point &p2);
+    Point Add2(Point &p1, Point &p2);
+    Point AddDirect(Point &p1, Point &p2);
+    Point Double(Point &p);
+    Point DoubleDirect(Point &p);
+
+    Point G;                 // Generator
+    Int   order;             // Curve order
+
     alignas(64) Point GTable[256*32];
-    Int order;
-    Point G;
 
 private:
-    uint8_t GetByte(std::string &str, int idx);
+    void SerializePublicKey(Point &pubKey, bool compressed, uint8_t *out33);
+    uint8_t GetByte(std::string &str,int idx);
+
 };
 
 #endif // SECP256K1H
