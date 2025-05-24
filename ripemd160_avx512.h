3df5eb7c0d0f5f9d3e767326a6a6c4817355325b
Aleksander <poczta@zielar.pl>
2025-05-24 20:24:28 +0200

    Update ripemd160_avx512.h

--- ripemd160_avx512.h
+++ ripemd160_avx512.h
@@ -2,23 +2,24 @@
 #define RIPEMD160_AVX512_H
 
 #include <immintrin.h>
+
 #include <cstdint>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// Pełny batch AVX-512: 16 bloków wejściowych po 64 bajty, 16 hashy po 20 bajtów
+// Full batch AVX-512: 16 blocks with 64 bits, 16 hashes with 20 bits
 void ripemd160_avx512_16blocks(const uint8_t* in[16], uint8_t* out[16]);
 
-// Batch 8x (wywołuje 16x, resztę zeruje)
+// Batch 8x
 void ripemd160_avx512_8blocks(const uint8_t* in[8], uint8_t* out[8]);
 
-// Batch 1x (wywołuje 16x, resztę zeruje)
+// Batch 1x
 void ripemd160_avx512_1block(const uint8_t* in, uint8_t* out);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif // RIPEMD160_AVX512_H
+#endif  // RIPEMD160_AVX512_H
