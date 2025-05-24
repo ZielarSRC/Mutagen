2c348b5cf0790a394f892b8f05d858f1d9f3f996
Aleksander <poczta@zielar.pl>
2025-05-24 02:53:22 +0200

    Update Int.h

--- Int.h
+++ Int.h
@@ -1,169 +1,386 @@
 #ifndef BIGINTH
 #define BIGINTH
 
-#include <immintrin.h>
-#include <inttypes.h>
 #include <string>
-#include <cstring>
+#include <inttypes.h>
+#include <immintrin.h>
+#include <stdint.h>
+#include <cstddef>
 
+// We need 1 extra block for Knuth div algorithm , Montgomery multiplication and ModInv
 #define BISIZE 256
 
-#if BISIZE == 256
-#define NB64BLOCK 5
-#define NB32BLOCK 10
-#elif BISIZE == 512
-#define NB64BLOCK 9
-#define NB32BLOCK 18
+#if BISIZE==256
+  #define NB64BLOCK 5
+  #define NB32BLOCK 10
+#elif BISIZE==512
+  #define NB64BLOCK 9
+  #define NB32BLOCK 18
 #else
-#error Unsupported size
+  #error Unsuported size
 #endif
 
 class Int {
- public:
+
+public:
+
   Int();
   Int(int64_t i64);
   Int(uint64_t u64);
-  Int(Int *a);
+  Int(const Int *a);
 
+  // Op
   void Add(uint64_t a);
-  void Add(Int *a);
-  void Add(Int *a, Int *b);
+  void Add(const Int *a);
+  void Add(const Int *a, const Int *b);
   void AddOne();
   void Sub(uint64_t a);
-  void Sub(Int *a);
-  void Sub(Int *a, Int *b);
+  void Sub(const Int *a);
+  void Sub(const Int *a, const Int *b);
   void SubOne();
-  void Mult(Int *a);
+  void Mult(const Int *a);
   uint64_t Mult(uint64_t a);
   uint64_t IMult(int64_t a);
-  uint64_t Mult(Int *a, uint64_t b);
-  uint64_t IMult(Int *a, int64_t b);
-  void Mult(Int *a, Int *b);
-  void Div(Int *a, Int *mod = NULL);
-  void MultModN(Int *a, Int *b, Int *n);
+  uint64_t Mult(const Int *a, uint64_t b);
+  uint64_t IMult(const Int *a, int64_t b);
+  void Mult(const Int *a, const Int *b);
+  void Div(const Int *a, Int *mod = nullptr);
+  void MultModN(const Int *a, const Int *b, const Int *n);
   void Neg();
   void Abs();
 
+  // Right shift (signed)
   void ShiftR(uint32_t n);
   void ShiftR32Bit();
   void ShiftR64Bit();
+  // Left shift
   void ShiftL(uint32_t n);
   void ShiftL32Bit();
   void ShiftL64Bit();
+  // Bit swap
   void SwapBit(int bitNumber);
   void Xor(const Int *a);
 
-  bool IsGreater(Int *a);
-  bool IsGreaterOrEqual(Int *a);
-  bool IsLowerOrEqual(Int *a);
-  bool IsLower(Int *a);
-  bool IsEqual(Int *a);
-  bool IsZero();
-  bool IsOne();
-  bool IsStrictPositive();
-  bool IsPositive();
-  bool IsNegative();
-  bool IsEven();
-  bool IsOdd();
-  bool IsProbablePrime();
+  // Comp 
+  bool IsGreater(const Int *a) const;
+  bool IsGreaterOrEqual(const Int *a) const;
+  bool IsLowerOrEqual(const Int *a) const;
+  bool IsLower(const Int *a) const;
+  bool IsEqual(const Int *a) const;
+  bool IsZero() const;
+  bool IsOne() const;
+  bool IsStrictPositive() const;
+  bool IsPositive() const;
+  bool IsNegative() const;
+  bool IsEven() const;
+  bool IsOdd() const;
 
-  double ToDouble();
+  double ToDouble() const;
 
-  static void SetupField(Int *n, Int *R = NULL, Int *R2 = NULL, Int *R3 = NULL, Int *R4 = NULL);
+  // Modular arithmetic
+
+  // Setup field
+  // n is the field characteristic
+  // R used in Montgomery mult (R = 2^size(n))
+  // R2 = R^2, R3 = R^3, R4 = R^4
+  static void SetupField(const Int *n, Int *R = nullptr, Int *R2 = nullptr, Int *R3 = nullptr, Int *R4 = nullptr);
   static Int *GetR();
   static Int *GetR2();
   static Int *GetR3();
   static Int *GetR4();
-  static Int *GetFieldCharacteristic();
-  void GCD(Int *a);
-  void Mod(Int *n);
+  static Int* GetFieldCharacteristic();
+
+  void GCD(const Int *a);                          // this <- GCD(this,a)
+  void Mod(const Int *n);                          // this <- this (mod n)
   void ModInv();
-  void MontgomeryMult(Int *a, Int *b);
-  void MontgomeryMult(Int *a);
-  void ModAdd(Int *a);
-  void ModAdd(Int *a, Int *b);
+  void MontgomeryMult(const Int *a, const Int *b);
+  void MontgomeryMult(const Int *a);
+  void ModAdd(const Int *a);
+  void ModAdd(const Int *a, const Int *b);
   void ModAdd(uint64_t a);
-  void ModSub(Int *a);
-  void ModSub(Int *a, Int *b);
+  void ModSub(const Int *a);
+  void ModSub(const Int *a, const Int *b);
   void ModSub(uint64_t a);
-  void ModMul(Int *a, Int *b);
-  void ModMul(Int *a);
-  void ModSquare(Int *a);
-  void ModCube(Int *a);
+  void ModMul(const Int *a, const Int *b);
+  void ModMul(const Int *a);
+  void ModSquare(const Int *a);
+  void ModCube(const Int *a);
   void ModDouble();
-  void ModExp(Int *e);
+  void ModExp(const Int *e);
   void ModNeg();
   void ModSqrt();
   bool HasSqrt();
-  void imm_umul_asm(const uint64_t *a, uint64_t b, uint64_t *res);
+  void imm_umul_asm(const uint64_t* a, uint64_t b, uint64_t* res);
 
-  // AVX-512 Optimized
-  void MontgomeryMultAVX512(Int *a, Int *b, Int *n);
-  void AddAVX512(Int *a);
-
-  // Secp256k1 specific
-  static void InitK1(Int *order);
-  void ModMulK1(Int *a, Int *b);
-  void ModMulK1(Int *a);
-  void ModSquareK1(Int *a);
-  void ModMulK1order(Int *a);
-  void ModAddK1order(Int *a, Int *b);
-  void ModAddK1order(Int *a);
-  void ModSubK1order(Int *a);
+  // Specific SecpK1
+  static void InitK1(const Int* order);
+  void ModMulK1(const Int *a, const Int *b);
+  void ModMulK1(const Int *a);
+  void ModSquareK1(const Int *a);
+  void ModMulK1order(const Int *a);
+  void ModAddK1order(const Int *a, const Int *b);
+  void ModAddK1order(const Int *a);
+  void ModSubK1order(const Int *a);
   void ModNegK1order();
   uint32_t ModPositiveK1();
 
-  int GetSize();
-  int GetSize64();
-  int GetBitLength();
+  // Size
+  int GetSize() const;       
+  int GetSize64() const;     
+  int GetBitLength() const;  
 
+  // Setter
   void SetInt32(uint32_t value);
-  void Set(Int *a);
-  void SetBase10(char *value);
-  void SetBase16(char *value);
-  void SetBaseN(int n, char *charset, char *value);
+  void Set(const Int *a);
+  void SetBase10(const char *value);
+  void SetBase16(const char *value);
+  void SetBaseN(int base, const char *charset, const char *value);
   void SetByte(int n, unsigned char byte);
   void SetDWord(int n, uint32_t b);
   void SetQWord(int n, uint64_t b);
   void Rand(int nbit);
-  void Rand(Int *randMax);
-  void Set32Bytes(unsigned char *bytes);
+  void Rand(const Int *randMax);
+  void Set32Bytes(const uint8_t *bytes);
   void MaskByte(int n);
 
-  uint32_t GetInt32();
-  int GetBit(uint32_t n);
-  unsigned char GetByte(int n);
-  void Get32Bytes(unsigned char *buff);
+  // Getter
+  uint32_t GetInt32() const;
+  int GetBit(uint32_t n) const;
+  unsigned char GetByte(int n) const;
+  void Get32Bytes(uint8_t *buff) const;
 
-  std::string GetBase2();
-  std::string GetBase10();
-  std::string GetBase16();
-  std::string GetBaseN(int n, char *charset);
-  std::string GetBlockStr();
-  std::string GetC64Str(int nbDigit);
+  // To String
+  std::string GetBase2() const;
+  std::string GetBase10() const;
+  std::string GetBase16() const;
+  std::string GetBaseN(int n, const char *charset) const;
+  std::string GetBlockStr() const;
+  std::string GetC64Str(int nbDigit) const;
 
+  // Check functions
   static void Check();
-  static bool CheckInv(Int *a);
+  static bool CheckInv(const Int *a);
   static Int P;
 
   union {
-    __attribute__((aligned(64))) uint32_t bits[NB32BLOCK];
-    __attribute__((aligned(64))) uint64_t bits64[NB64BLOCK];
+    uint32_t bits[NB32BLOCK];
+    uint64_t bits64[NB64BLOCK];
   };
 
- private:
+private:
+
   void MatrixVecMul(Int *u, Int *v, int64_t _11, int64_t _12, int64_t _21, int64_t _22, uint64_t *cu, uint64_t *cv);
-  void MatrixVecMul(Int *u, Int *v, int64_t _11, int64_t _12, int64_t _21, int64_t _22);
-  uint64_t AddCh(Int *a, uint64_t ca, Int *b, uint64_t cb);
-  uint64_t AddCh(Int *a, uint64_t ca);
-  uint64_t AddC(Int *a);
-  void AddAndShift(Int *a, Int *b, uint64_t cH);
-  void ShiftL64BitAndSub(Int *a, int n);
-  uint64_t Mult(Int *a, uint32_t b);
-  int GetLowestBit();
+  void MatrixVecMul(Int* u, Int* v, int64_t _11, int64_t _12, int64_t _21, int64_t _22);
+  uint64_t AddCh(const Int *a, uint64_t ca, const Int *b, uint64_t cb);
+  uint64_t AddCh(const Int *a, uint64_t ca);
+  uint64_t AddC(const Int *a);
+  void AddAndShift(const Int *a, const Int *b, uint64_t cH);
+  void ShiftL64BitAndSub(const Int *a, int n);
+  int  GetLowestBit() const;
   void CLEAR();
   void CLEARFF();
-  void DivStep62(Int *u, Int *v, int64_t *eta, int *pos, int64_t *uu, int64_t *uv, int64_t *vu, int64_t *vv);
+  void DivStep62(const Int* u, const Int* v, int64_t* eta, int *pos, int64_t* uu, int64_t* uv, int64_t* vu, int64_t* vv);
+
 };
 
-#endif  // BIGINTH
+// Inline routines
+
+#ifndef WIN64
+
+// Missing intrinsics
+inline uint64_t _umul128(uint64_t a, uint64_t b, uint64_t *h)
+{
+#if defined(__BMI2__)
+    uint64_t rlo, rhi;
+    __asm__ (
+        "mulx %[B], %[LO], %[HI]"   
+        : [LO]"=r"(rlo), [HI]"=r"(rhi)
+        : "d"(a), [B]"r"(b)
+        : "cc"  
+    );
+    *h = rhi;
+    return rlo;
+#else
+    uint64_t rhi, rlo;
+    __asm__ (
+        "mulq %[B]"
+        : "=d"(rhi), "=a"(rlo)
+        : "a"(a), [B]"r"(b)
+        : "cc"
+    );
+    *h = rhi;
+    return rlo;
+#endif
+}
+
+
+inline int64_t _mul128(int64_t a, int64_t b, int64_t *h) {
+  uint64_t rhi;
+  uint64_t rlo;
+  __asm__( "imulq  %[b];" :"=d"(rhi),"=a"(rlo) :"1"(a),[b]"rm"(b));
+  *h = rhi;
+  return rlo;  
+}
+
+static inline uint64_t _udiv128(uint64_t hi, uint64_t lo, uint64_t d, uint64_t *r)
+{
+    uint64_t q;    
+    uint64_t rem;  
+
+    asm (
+        "divq %4"
+        : "=d"(rem), "=a"(q)
+        : "a"(lo), "d"(hi), "r"(d)
+        : "cc"
+    );
+
+    *r = rem;
+    return q;
+}
+
+static inline uint64_t my_rdtsc() {
+  uint32_t h;
+  uint32_t l;
+  __asm__( "rdtsc;" :"=d"(h),"=a"(l));
+  return (uint64_t)h << 32 | (uint64_t)l;
+}
+
+#define __shiftright128(a,b,n) ((a)>>(n))|((b)<<(64-(n)))
+#define __shiftleft128(a,b,n) ((b)<<(n))|((a)>>(64-(n)))
+
+
+#define _subborrow_u64(a,b,c,d) __builtin_ia32_sbb_u64(a,b,c,(long long unsigned int*)d);
+#define _addcarry_u64(a,b,c,d) __builtin_ia32_addcarryx_u64(a,b,c,(long long unsigned int*)d);
+#define _byteswap_uint64 __builtin_bswap64
+#define LZC(x) __builtin_clzll(x)
+#define TZC(x) __builtin_ctzll(x)
+
+#else
+
+#include <intrin.h>
+#define TZC(x) _tzcnt_u64(x)
+#define LZC(x) _lzcnt_u64(x)
+
+#endif
+
+
+#define LoadI64(i,i64)    \
+i.bits64[0] = i64;        \
+i.bits64[1] = i64 >> 63;  \
+i.bits64[2] = i.bits64[1];\
+i.bits64[3] = i.bits64[1];\
+i.bits64[4] = i.bits64[1];
+
+static inline void imm_mul(const uint64_t *x, uint64_t y, uint64_t *dst, uint64_t *carryH) {
+
+  unsigned char c = 0;
+  uint64_t h, carry;
+  dst[0] = _umul128(x[0], y, &h); carry = h;
+  c = _addcarry_u64(c, _umul128(x[1], y, &h), carry, dst + 1); carry = h;
+  c = _addcarry_u64(c, _umul128(x[2], y, &h), carry, dst + 2); carry = h;
+  c = _addcarry_u64(c, _umul128(x[3], y, &h), carry, dst + 3); carry = h;
+  c = _addcarry_u64(c, _umul128(x[4], y, &h), carry, dst + 4); carry = h;
+#if NB64BLOCK > 5
+  c = _addcarry_u64(c, _umul128(x[5], y, &h), carry, dst + 5); carry = h;
+  c = _addcarry_u64(c, _umul128(x[6], y, &h), carry, dst + 6); carry = h;
+  c = _addcarry_u64(c, _umul128(x[7], y, &h), carry, dst + 7); carry = h;
+  c = _addcarry_u64(c, _umul128(x[8], y, &h), carry, dst + 8); carry = h;
+#endif
+  *carryH = carry;
+
+}
+
+static inline void imm_imul(const uint64_t *x, uint64_t y, uint64_t *dst, uint64_t *carryH) {
+
+  unsigned char c = 0;
+  uint64_t h,carry;
+  dst[0] = _umul128(x[0],y,&h); carry = h;
+  c = _addcarry_u64(c,_umul128(x[1],y,&h),carry,dst + 1); carry = h;
+  c = _addcarry_u64(c,_umul128(x[2],y,&h),carry,dst + 2); carry = h;
+  c = _addcarry_u64(c,_umul128(x[3],y,&h),carry,dst + 3); carry = h;
+#if NB64BLOCK > 5
+  c = _addcarry_u64(c,_umul128(x[4],y,&h),carry,dst + 4); carry = h;
+  c = _addcarry_u64(c,_umul128(x[5],y,&h),carry,dst + 5); carry = h;
+  c = _addcarry_u64(c,_umul128(x[6],y,&h),carry,dst + 6); carry = h;
+  c = _addcarry_u64(c,_umul128(x[7],y,&h),carry,dst + 7); carry = h;
+#endif
+  c = _addcarry_u64(c,_mul128(x[NB64BLOCK - 1],y,(int64_t*)&h),carry,dst + NB64BLOCK - 1); carry = h;
+  * carryH = carry;
+
+}
+
+static inline void imm_umul(const uint64_t *x, uint64_t y, uint64_t *dst) {
+
+  // Assume that x[NB64BLOCK-1] is 0
+  unsigned char c = 0;
+  uint64_t h, carry;
+  dst[0] = _umul128(x[0], y, &h); carry = h;
+  c = _addcarry_u64(c, _umul128(x[1], y, &h), carry, dst + 1); carry = h;
+  c = _addcarry_u64(c, _umul128(x[2], y, &h), carry, dst + 2); carry = h;
+  c = _addcarry_u64(c, _umul128(x[3], y, &h), carry, dst + 3); carry = h;
+#if NB64BLOCK > 5
+  c = _addcarry_u64(c, _umul128(x[4], y, &h), carry, dst + 4); carry = h;
+  c = _addcarry_u64(c, _umul128(x[5], y, &h), carry, dst + 5); carry = h;
+  c = _addcarry_u64(c, _umul128(x[6], y, &h), carry, dst + 6); carry = h;
+  c = _addcarry_u64(c, _umul128(x[7], y, &h), carry, dst + 7); carry = h;
+#endif
+  _addcarry_u64(c, 0ULL, carry, dst + (NB64BLOCK - 1));
+
+}
+
+static inline void shiftR(unsigned char n, uint64_t *d) {
+
+  d[0] = __shiftright128(d[0], d[1], n);
+  d[1] = __shiftright128(d[1], d[2], n);
+  d[2] = __shiftright128(d[2], d[3], n);
+  d[3] = __shiftright128(d[3], d[4], n);
+#if NB64BLOCK > 5
+  d[4] = __shiftright128(d[4], d[5], n);
+  d[5] = __shiftright128(d[5], d[6], n);
+  d[6] = __shiftright128(d[6], d[7], n);
+  d[7] = __shiftright128(d[7], d[8], n);
+#endif
+  d[NB64BLOCK-1] = ((int64_t)d[NB64BLOCK-1]) >> n;
+
+}
+
+static inline void shiftR(unsigned char n, uint64_t *d, uint64_t h) {
+
+  d[0] = __shiftright128(d[0],d[1],n);
+  d[1] = __shiftright128(d[1],d[2],n);
+  d[2] = __shiftright128(d[2],d[3],n);
+  d[3] = __shiftright128(d[3],d[4],n);
+#if NB64BLOCK > 5
+  d[4] = __shiftright128(d[4],d[5],n);
+  d[5] = __shiftright128(d[5],d[6],n);
+  d[6] = __shiftright128(d[6],d[7],n);
+  d[7] = __shiftright128(d[7],d[8],n);
+#endif
+
+  d[NB64BLOCK-1] = __shiftright128(d[NB64BLOCK-1],h,n);
+
+}
+
+static inline void shiftL(unsigned char n, uint64_t *d) {
+
+#if NB64BLOCK > 5
+  d[8] = __shiftleft128(d[7], d[8], n);
+  d[7] = __shiftleft128(d[6], d[7], n);
+  d[6] = __shiftleft128(d[5], d[6], n);
+  d[5] = __shiftleft128(d[4], d[5], n);
+#endif
+  d[4] = __shiftleft128(d[3], d[4], n);
+  d[3] = __shiftleft128(d[2], d[3], n);
+  d[2] = __shiftleft128(d[1], d[2], n);
+  d[1] = __shiftleft128(d[0], d[1], n);
+  d[0] = d[0] << n;
+
+}
+
+static inline int isStrictGreater128(uint64_t h1, uint64_t l1, uint64_t h2, uint64_t l2) {
+  if(h1>h2) return 1;
+  if(h1==h2) return l1>l2;
+  return 0;
+}
+
+#endif // BIGINTH
