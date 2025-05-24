d7d323d671cdb6f0c51917fc9c031f61a022bae5
Aleksander <poczta@zielar.pl>
2025-05-24 02:54:42 +0200

    Update IntGroup.h

--- IntGroup.h
+++ IntGroup.h
@@ -1,37 +1,20 @@
-#include <immintrin.h>
-#include <stdlib.h>
-#include "IntGroup.h"
+#ifndef INTGROUPH
+#define INTGROUPH
 
-IntGroup::IntGroup(int size) {
-  this->size = size;
-  subp = (Int *)aligned_alloc(64, size * sizeof(Int));  // 64B alignment for AVX-512
-}
+#include "Int.h"
 
-IntGroup::~IntGroup() {
-  free(subp);
-}
+class IntGroup {
+ public:
+  IntGroup(int size);
+  ~IntGroup();
 
-void IntGroup::Set(Int *pts) {
-  ints = pts;
-}
+  void Set(Int *pts);
+  __attribute__((target("avx512f"))) void ModInv();  // AVX-512 batch inversion
 
-// Batch modular inverse using AVX-512 vectorized operations
-void IntGroup::ModInv() {
-  Int inverse;
-  subp[0].Set(&ints[0]);
-  // Forward pass
-  for (int i = 1; i < size; i++)
-    subp[i].ModMulK1(&subp[i - 1], &ints[i]);  // Uses AVX-512 FMA
+ private:
+  Int *ints;
+  Int *subp;
+  int size;
+};
 
-  inverse.Set(&subp[size - 1]);
-  inverse.ModInv();  // AVX-512 accelerated inverse
-
-  // Backward pass
-  for (int i = size - 1; i > 0; i--) {
-    Int tmp;
-    tmp.ModMulK1(&subp[i - 1], &inverse);
-    inverse.ModMulK1(&ints[i]);
-    ints[i].Set(&tmp);
-  }
-  ints[0].Set(&inverse);
-}
+#endif  // INTGROUPH
