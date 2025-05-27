92e93f0199413f5915d305536376a224090369c3
Aleksander <poczta@zielar.pl>
2025-05-27 15:29:50 +0200

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -1,10 +1,13 @@
 #include "Point.h"
 
 Point::Point() {
+  x.SetInt32(0);
+  y.SetInt32(0);
+  z.SetInt32(0);
 }
 
 Point::Point(const Point &p) {
-  x.Set((Int *)&p.x);  // Uses AVX-512 optimized copy
+  x.Set((Int *)&p.x);
   y.Set((Int *)&p.y);
   z.Set((Int *)&p.z);
 }
@@ -18,10 +21,22 @@ Point::Point(Int *cx, Int *cy, Int *cz) {
 Point::Point(Int *cx, Int *cz) {
   x.Set(cx);
   z.Set(cz);
+  y.SetInt32(0);
+}
+
+Point::~Point() {}
+
+Point &Point::operator=(const Point &other) {
+  if (this != &other) {
+    x.Set((Int *)&other.x);
+    y.Set((Int *)&other.y);
+    z.Set((Int *)&other.z);
+  }
+  return *this;
 }
 
 void Point::Clear() {
-  x.SetInt32(0);  // AVX-512 optimized zeroing
+  x.SetInt32(0);
   y.SetInt32(0);
   z.SetInt32(0);
 }
@@ -38,21 +53,18 @@ void Point::Set(Int *cx, Int *cy, Int *cz) {
   z.Set(cz);
 }
 
-bool Point::isZero() {
-  return x.IsZero() && y.IsZero();  // AVX-512 vectorized check
-}
+bool Point::IsZero() { return z.IsZero(); }
+
+bool Point::IsEqual(Point &p) { return x.IsEqual(&p.x) && y.IsEqual(&p.y) && z.IsEqual(&p.z); }
 
 void Point::Reduce() {
+  if (z.IsZero()) {
+    return;  // Point at infinity
+  }
+
   Int i(&z);
-  i.ModInv();  // AVX-512 accelerated inverse
-  x.ModMul(&x, &i);
-  y.ModMul(&y, &i);
+  i.ModInv();
+  x.ModMulK1(&x, &i);
+  y.ModMulK1(&y, &i);
   z.SetInt32(1);
-}
-
-bool Point::equals(Point &p) {
-  return x.IsEqual(&p.x) && y.IsEqual(&p.y) && z.IsEqual(&p.z);  // Vectorized comparison
-}
-
-Point::~Point() {
 }
