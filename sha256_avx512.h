ac91e2a6457f6e133ba12b1df931f34b905b859b
Aleksander <poczta@zielar.pl>
2025-05-27 16:32:29 +0200

    Update sha256_avx512.h

--- sha256_avx512.h
+++ sha256_avx512.h
@@ -2,29 +2,30 @@
 #define SHA256_AVX512_H
 
 #include <immintrin.h>
-#include <cstdint>
+#include <stdint.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+class SHA256_AVX512 {
+ public:
+  SHA256_AVX512();
+  ~SHA256_AVX512();
+  void Init();
+  void InitTable();
+  int Check512();
+  void Transform(uint64_t* input64, uint32_t* state, uint32_t rounds = 1);
+  static void PrecalcTable();
 
-// Maximum performance AVX-512: batch 32 blocks (64B for block, 32x32B hash)
-void sha256_avx512_32blocks(const uint8_t* in[32], uint8_t* out[32]);
+  // Static SHA256 functions from original
+  static void Init(uint32_t* h);
+  static void Transform(uint32_t* state, const uint32_t* block);
+  static void Transform(uint32_t* state, const uint32_t* block, uint32_t nbBlocks);
+  static void Compress(uint32_t* state, const uint32_t* block);
+  static uint32_t _K[64];
 
-// Batch 16x
-void sha256_avx512_16blocks(const uint8_t* in[16], uint8_t* out[16]);
+ private:
+  __m512i _w512[16];
+  uint32_t __attribute__((aligned(64))) _w[16][16];
+  __m512i* __attribute__((aligned(64))) w512;
+  static __m512i* K512;
+};
 
-// Batch 8x
-void sha256_avx512_8blocks(const uint8_t* in[8], uint8_t* out[8]);
-
-// Batch 4x
-void sha256_avx512_4blocks(const uint8_t* in[4], uint8_t* out[4]);
-
-// Batch 1x
-void sha256_avx512_1block(const uint8_t* in, uint8_t* out);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif // SHA256_AVX512_H
+#endif  // SHA256_AVX512_H
