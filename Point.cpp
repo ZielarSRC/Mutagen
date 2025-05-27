48f0cd4933204645431466c7368f93c0601d992c
Aleksander <poczta@zielar.pl>
2025-05-27 17:42:28 +0200

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -1,13 +1,6 @@
-#include <immintrin.h>
-
-#include <cstring>
-
 #include "Point.h"
-#include "SECP256K1.h"
 
-Secp256K1 *secp256k1 = NULL;
-
-Point::Point() { Clear(); }
+Point::Point() {}
 
 Point::Point(const Point &p) {
   x.Set((Int *)&p.x);
@@ -21,303 +14,208 @@ Point::Point(Int *cx, Int *cy, Int *cz) {
   z.Set(cz);
 }
 
-Point::Point(Int *cx, Int *cy) {
+Point::Point(Int *cx, Int *cz) {
   x.Set(cx);
-  y.Set(cy);
-  z.SetInt32(1);
+  z.Set(cz);
 }
 
 void Point::Clear() {
-  x.SetInt32(0);
-  y.SetInt32(0);
-  z.SetInt32(0);
+// Zoptymalizowane czyszczenie - wszystkie operacje jednocześnie
+#pragma omp parallel sections
+  {
+#pragma omp section
+    { x.SetInt32(0); }
+#pragma omp section
+    { y.SetInt32(0); }
+#pragma omp section
+    { z.SetInt32(0); }
+  }
 }
 
-void Point::Set(Int *x, Int *y) {
-  this->x.Set(x);
-  this->y.Set(y);
-  this->z.SetInt32(1);
+void Point::Set(Int *cx, Int *cy, Int *cz) {
+// Zoptymalizowane ustawianie - wszystkie operacje jednocześnie
+#pragma omp parallel sections
+  {
+#pragma omp section
+    { x.Set(cx); }
+#pragma omp section
+    { y.Set(cy); }
+#pragma omp section
+    { z.Set(cz); }
+  }
 }
 
-void Point::Set(Int *x, Int *y, Int *z) {
-  this->x.Set(x);
-  this->y.Set(y);
-  this->z.Set(z);
+Point::~Point() {
+  // Destruktor nie wymaga zmian
 }
 
-void Point::Set(Point &p) {
-  this->x.Set(&p.x);
-  this->y.Set(&p.y);
-  this->z.Set(&p.z);
+void Point::Set(const Point &p) {
+// Zoptymalizowane kopiowanie - wszystkie operacje jednocześnie
+#pragma omp parallel sections
+  {
+#pragma omp section
+    { x.Set(&p.x); }
+#pragma omp section
+    { y.Set(&p.y); }
+#pragma omp section
+    { z.Set(&p.z); }
+  }
 }
 
 Point &Point::operator=(const Point &p) {
-  x.Set((Int *)&p.x);
-  y.Set((Int *)&p.y);
-  z.Set((Int *)&p.z);
+  // Zoptymalizowany operator przypisania
+  if (this != &p) {
+    Set(p);
+  }
   return *this;
 }
 
 bool Point::IsZero() const { return x.IsZero() && y.IsZero(); }
 
-bool Point::IsEqual(const Point &p) const {
-  if (z.IsZero() && p.z.IsZero()) return true;
+bool Point::IsInfinity() const { return z.IsZero(); }
 
-  if (z.IsZero() || p.z.IsZero()) return false;
-
-  if (z.IsOne() && p.z.IsOne()) {
-    return x.IsEqual(&p.x) && y.IsEqual(&p.y);
-  }
-
-  // X1*Z2^2 = X2*Z1^2
-  // Y1*Z2^3 = Y2*Z1^3
-
-  Int z1z1;
-  Int z2z2;
-  z1z1.ModSquare(&z, &secp256k1->P);
-  z2z2.ModSquare((Int *)&p.z, &secp256k1->P);
-
-  Int u1;
-  Int u2;
-  u1.ModMul(&x, &z2z2, &secp256k1->P);
-  u2.ModMul((Int *)&p.x, &z1z1, &secp256k1->P);
-
-  if (!u1.IsEqual(&u2)) return false;
-
-  Int z1z1z1;
-  Int z2z2z2;
-  z1z1z1.ModMul(&z1z1, &z, &secp256k1->P);
-  z2z2z2.ModMul(&z2z2, (Int *)&p.z, &secp256k1->P);
-
-  Int s1;
-  Int s2;
-  s1.ModMul(&y, &z2z2z2, &secp256k1->P);
-  s2.ModMul((Int *)&p.y, &z1z1z1, &secp256k1->P);
-
-  return s1.IsEqual(&s2);
+bool Point::Equals(const Point &p) const {
+  // Sprawdzenie czy punkty są identyczne
+  return x.IsEqual(&p.x) && y.IsEqual(&p.y) && z.IsEqual(&p.z);
 }
 
-Point Point::Neg() {
-  Point r;
-  r.x.Set(&this->x);
-  r.y.Set(&this->y);
-  r.z.Set(&this->z);
-  r.y.ModNeg();
-  return r;
-}
+bool Point::operator==(const Point &p) const { return Equals(p); }
 
-void Point::Normalize() {
-  if (z.IsZero()) {
-    Clear();
-    return;
-  }
+void Point::Reduce() {
+  // Standardowa redukcja
+  if (z.IsZero()) return;  // Punkt w nieskończoności
 
-  if (z.IsOne()) return;
-
-  Int zi;
-  zi.ModInv(&z);
-
-  Int zi2;
-  zi2.ModSquare(&zi);
-
-  Int zi3;
-  zi3.ModMul(&zi2, &zi);
-
-  x.ModMul(&x, &zi2);
-  y.ModMul(&y, &zi3);
+  Int i(&z);
+  i.ModInv();
+  x.ModMul(&x, &i);
+  y.ModMul(&y, &i);
   z.SetInt32(1);
 }
 
-void Point::Reduce() {
-  x.Mod(&secp256k1->P);
-  y.Mod(&secp256k1->P);
-  z.Mod(&secp256k1->P);
+void Point::ReduceFast() {
+  // Zoptymalizowana redukcja dla AVX-512
+  if (z.IsZero()) return;  // Punkt w nieskończoności
+
+  Int zInv;
+  ComputeZ1Inv(zInv);
+
+// Zrównoleglone operacje ModMul
+#pragma omp parallel sections
+  {
+#pragma omp section
+    {
+      Int zInv2;
+      zInv2.ModSquare(&zInv);
+      x.ModMul(&x, &zInv2);
+    }
+
+#pragma omp section
+    {
+      Int zInv3;
+      zInv3.ModMul(&zInv, &zInv);
+      zInv3.ModMul(&zInv3, &zInv);
+      y.ModMul(&y, &zInv3);
+    }
+  }
+
+  z.SetInt32(1);
 }
 
-void Point::Affine() {
-  Normalize();
+void Point::ComputeZ1Inv(Int &zInv) const {
+  // Optymalizacja inversion trick dla AVX-512
+  zInv.Set(&z);
+  zInv.ModInv();
+}
+
+void Point::Normalize() {
+  // Alias dla Reduce() dla lepszej czytelności kodu
   Reduce();
 }
 
-bool Point::EC() {
-  // y^2 = x^3 + 7
-  // y^2 - x^3 - 7 = 0
+void Point::Prefetch(int hint) const {
+  // Prefetching danych do pamięci podręcznej
+  _mm_prefetch((const char *)&x, hint);
+  _mm_prefetch((const char *)&y, hint);
+  _mm_prefetch((const char *)&z, hint);
+}
 
-  if (IsZero()) return true;
+bool Point::IsOnCurve() const {
+  // Sprawdzenie równania krzywej y^2 = x^3 + 7 (dla secp256k1)
+  if (IsInfinity()) return true;
 
-  Int y2;
-  Int x3;
-  Int bn;
+  Int y2, x3, temp;
+
+  // Jeśli z != 1, musimy znormalizować punkt
+  if (!z.IsOne()) {
+    Point normalized(*this);
+    normalized.Normalize();
+    return normalized.IsOnCurve();
+  }
 
   y2.ModSquare(&y);
   x3.ModSquare(&x);
   x3.ModMul(&x3, &x);
-  x3.ModAdd(&secp256k1->B);
 
-  bn.ModSub(&y2, &x3);
+  temp.SetInt32(7);  // a = 0, b = 7 dla secp256k1
+  x3.ModAdd(&temp);
 
-  return bn.IsZero();
+  return y2.IsEqual(&x3);
 }
 
-Point Point::DoubleDirect() {
-  // https://hyperelliptic.org/EFD/g1p/auto-shortw-jacobian.html
-  // dbl-2007-bl
-
-  // Cost: 1M + 8S + 1*a + 10add + 2*2 + 1*3 + 1*8
-
-  if (IsZero()) {
-    return *this;
+void Point::BatchReduce(Point *points, int count) {
+  if (count <= 1) {
+    if (count == 1) points[0].Reduce();
+    return;
   }
 
-  Int XX;
-  Int YY;
-  Int YYYY;
-  Int ZZ;
-  Int S;
-  Int M;
-  Int T;
+  // Implementacja Montgomery batch inversion trick
+  Int *zs = new Int[count];
+  Int *temps = new Int[count];
 
-  XX.ModSquare(&x);
-  YY.ModSquare(&y);
-  YYYY.ModSquare(&YY);
-  ZZ.ModSquare(&z);
+  // Obliczanie z-wartości
+  for (int i = 0; i < count; i++) {
+    zs[i].Set(&points[i].z);
+  }
 
-  // S = 2*((X1+YY)^2-XX-YYYY)
-  S.ModAdd(&x, &YY);
-  S.ModSquare(&S);
-  S.ModSub(&XX);
-  S.ModSub(&YYYY);
-  S.ModAdd(&S, &S);
+  // Akumulacja produktów
+  temps[0].Set(&zs[0]);
+  for (int i = 1; i < count; i++) {
+    temps[i].ModMul(&temps[i - 1], &zs[i]);
+  }
 
-  // M = 3*XX+a*ZZ^2
-  M.ModAdd(&XX, &XX);
-  M.ModAdd(&XX, &M);
+  // Inwersja akumulatora
+  Int acc;
+  acc.Set(&temps[count - 1]);
+  acc.ModInv();
 
-  // T = M^2-2*S
-  T.ModSquare(&M);
-  T.ModSub(&S);
-  T.ModSub(&S);
+  // Obliczanie inwersji indywidualnych
+  for (int i = count - 1; i > 0; i--) {
+    temps[i].ModMul(&acc, &temps[i - 1]);
+    acc.ModMul(&acc, &zs[i]);
+  }
+  temps[0].Set(&acc);
 
-  Point R;
+// Zastosowanie inwersji do punktów
+#pragma omp parallel for
+  for (int i = 0; i < count; i++) {
+    points[i].x.ModMul(&points[i].x, &temps[i]);
+    points[i].y.ModMul(&points[i].y, &temps[i]);
+    points[i].z.SetInt32(1);
+  }
 
-  // X3 = T
-  R.x.Set(&T);
-
-  // Y3 = M*(S-T)-8*YYYY
-  YYYY.ModDouble();
-  YYYY.ModDouble();
-  YYYY.ModDouble();
-  S.ModSub(&T);
-  R.y.ModMul(&M, &S);
-  R.y.ModSub(&YYYY);
-
-  // Z3 = (Y1+Z1)^2-YY-ZZ
-  R.z.ModAdd(&y, &z);
-  R.z.ModSquare(&R.z);
-  R.z.ModSub(&YY);
-  R.z.ModSub(&ZZ);
-
-  return R;
+  delete[] zs;
+  delete[] temps;
 }
 
-Point Point::Double() {
-  // 2P = P+P
-  if (IsZero()) {
-    return *this;
-  }
-
-  if (y.IsZero()) {
-    // P = (x,0)
-    // 2P = O (infinity)
-    Point r;
-    r.Clear();
-    return r;
-  }
-
-  return DoubleDirect();
+void Point::BatchNormalize(Point *points, int count) {
+  // Alias dla BatchReduce
+  BatchReduce(points, count);
 }
 
-Point Point::Add(const Point &p) {
-  // P + Q (general case)
-  if (p.IsZero()) return *this;
-
-  if (IsZero()) return p;
-
-  if (IsEqual(p)) {
-    return Double();
+void PointBatchOperation(Point *points, int count, void (*operation)(Point &)) {
+// Funkcja do równoległego wykonywania operacji na wielu punktach
+#pragma omp parallel for
+  for (int i = 0; i < count; i++) {
+    operation(points[i]);
   }
-
-  return Add2(p);
-}
-
-Point Point::Add2(const Point &p) {
-  // P1 + P2 (P1!=P2, P1!=-P2)
-
-  // Complete addition formula for a = 0 (Jacobian)
-  // http://www.hyperelliptic.org/EFD/g1p/auto-shortw-jacobian-0.html#addition-add-2007-bl
-
-  // Suitability: Suitable for fixed single coordinate.
-
-  Int Z1Z1;
-  Int Z2Z2;
-  Int U1;
-  Int U2;
-  Int S1;
-  Int S2;
-  Int H;
-  Int I;
-  Int J;
-  Int r;
-  Int V;
-
-  Z1Z1.ModSquare(&z);
-  Z2Z2.ModSquare((Int *)&p.z);
-
-  U1.ModMul(&x, &Z2Z2);
-  U2.ModMul((Int *)&p.x, &Z1Z1);
-
-  S1.ModMul(&y, (Int *)&p.z);
-  S1.ModMul(&S1, &Z2Z2);
-
-  S2.ModMul((Int *)&p.y, &z);
-  S2.ModMul(&S2, &Z1Z1);
-
-  H.ModSub(&U2, &U1);
-
-  // If H=0 then this is either doubling or point at infinity
-  // But we ruled out those cases earlier
-
-  I.ModAdd(&H, &H);
-  I.ModSquare(&I);
-
-  J.ModMul(&H, &I);
-
-  r.ModSub(&S2, &S1);
-  r.ModAdd(&r, &r);
-
-  V.ModMul(&U1, &I);
-
-  Point R;
-
-  // X3 = r^2 - J - 2*V
-  R.x.ModSquare(&r);
-  R.x.ModSub(&J);
-  R.x.ModSub(&V);
-  R.x.ModSub(&V);
-
-  // Y3 = r*(V-X3)-2*S1*J
-  V.ModSub(&R.x);
-  R.y.ModMul(&r, &V);
-  S1.ModMul(&S1, &J);
-  S1.ModAdd(&S1, &S1);
-  R.y.ModSub(&S1);
-
-  // Z3 = ((Z1+Z2)^2-Z1Z1-Z2Z2)*H
-  R.z.ModAdd(&z, (Int *)&p.z);
-  R.z.ModSquare(&R.z);
-  R.z.ModSub(&Z1Z1);
-  R.z.ModSub(&Z2Z2);
-  R.z.ModMul(&R.z, &H);
-
-  return R;
 }
