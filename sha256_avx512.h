bc9ae98737d4ae1d0bf8937a81ce177c70b27fb2
Aleksander <poczta@zielar.pl>
2025-05-27 17:58:28 +0200

    Update sha256_avx512.h

--- sha256_avx512.h
+++ sha256_avx512.h
@@ -2,30 +2,34 @@
 #define SHA256_AVX512_H
 
 #include <immintrin.h>
-#include <stdint.h>
 
-class SHA256_AVX512 {
- public:
-  SHA256_AVX512();
-  ~SHA256_AVX512();
-  void Init();
-  void InitTable();
-  int Check512();
-  void Transform(uint64_t* input64, uint32_t* state, uint32_t rounds = 1);
-  static void PrecalcTable();
+#include <cstdint>
 
-  // Static SHA256 functions from original
-  static void Init(uint32_t* h);
-  static void Transform(uint32_t* state, const uint32_t* block);
-  static void Transform(uint32_t* state, const uint32_t* block, uint32_t nbBlocks);
-  static void Compress(uint32_t* state, const uint32_t* block);
-  static uint32_t _K[64];
+// Process 8 hashes in parallel (same interface as AVX2 version for compatibility)
+void sha256avx512_8B(const uint8_t* data0, const uint8_t* data1, const uint8_t* data2,
+                     const uint8_t* data3, const uint8_t* data4, const uint8_t* data5,
+                     const uint8_t* data6, const uint8_t* data7, unsigned char* hash0,
+                     unsigned char* hash1, unsigned char* hash2, unsigned char* hash3,
+                     unsigned char* hash4, unsigned char* hash5, unsigned char* hash6,
+                     unsigned char* hash7);
 
- private:
-  __m512i _w512[16];
-  uint32_t __attribute__((aligned(64))) _w[16][16];
-  __m512i* __attribute__((aligned(64))) w512;
-  static __m512i* K512;
-};
+// Process 16 hashes in parallel (new AVX-512 optimized function)
+void sha256avx512_16B(const uint8_t* data[16], unsigned char* hash[16]);
+
+// Process 32 hashes in parallel for even higher throughput
+void sha256avx512_32B(const uint8_t* data[32], unsigned char* hash[32]);
+
+// Batch processing with optimal threading for Xeon 8488C
+void sha256avx512_batch(const uint8_t** data, unsigned char** hash, size_t num_inputs);
+
+// Stream processing for continuous data
+void sha256avx512_stream(const uint8_t** data_blocks, size_t num_blocks, size_t block_size,
+                         unsigned char** hashes);
+
+// Get optimal thread count for the current system
+int sha256avx512_get_optimal_threads();
+
+// Prefetch data into cache levels
+void sha256avx512_prefetch(const void* ptr, int hint);
 
 #endif  // SHA256_AVX512_H
