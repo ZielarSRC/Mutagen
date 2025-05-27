97d8b26fa4bbaa120da18129d96027974a07833f
Aleksander <poczta@zielar.pl>
2025-05-27 16:30:37 +0200

    Update IntGroup.h

--- IntGroup.h
+++ IntGroup.h
@@ -10,11 +10,15 @@ class IntGroup {
 
   void Set(Int *pts);
   __attribute__((target("avx512f"))) void ModInv();  // AVX-512 batch inversion
+  Int Get(Int *a);
+  void InitP();
 
  private:
   Int *ints;
   Int *subp;
   int size;
+  bool initialized;
+  Int P;
 };
 
 #endif  // INTGROUPH
