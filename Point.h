c829bb8b23d1b7f09a213cb2ff1913c65e845fc2
Aleksander <poczta@zielar.pl>
2025-05-27 15:30:16 +0200

    Update Point.h

--- Point.h
+++ Point.h
@@ -1,27 +1,30 @@
-#ifndef POINTH
-#define POINTH
+#ifndef POINT_H
+#define POINT_H
 
 #include "Int.h"
 
 class Point {
-public:
-    Int x, y, z;
+ public:
+  Int x, y, z;
 
-    Point();
-    Point(const Point &p);
-    Point(Int *cx, Int *cy, Int *cz);
-    Point(Int *cx, Int *cz);
-    ~Point();
+  Point();
+  Point(const Point &p);
+  Point(Int *cx, Int *cy, Int *cz);
+  Point(Int *cx, Int *cz);
+  ~Point();
 
-    bool isZero();
-    bool equals(Point &p);
-    void Set(Point &p);
-    void Set(Int *cx, Int *cy, Int *cz);
-    void Clear();
-    void Reduce();
+  bool IsZero();
+  bool IsEqual(Point &p);
+  void Set(Point &p);
+  void Set(Int *cx, Int *cy, Int *cz);
+  void Clear();
+  void Reduce();
 
-    // AVX-512 optimizations
-    friend class Secp256K1;
+  // Assignment operator
+  Point &operator=(const Point &other);
+
+  // AVX-512 optimizations
+  friend class Secp256K1;
 };
 
-#endif // POINTH
+#endif  // POINT_H
