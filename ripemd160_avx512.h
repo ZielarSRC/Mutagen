12aca71ed59b7b30fcc17b7b9c827bef7b681459
Aleksander <poczta@zielar.pl>
2025-05-27 16:31:14 +0200

    Update ripemd160_avx512.h

--- ripemd160_avx512.h
+++ ripemd160_avx512.h
@@ -2,24 +2,19 @@
 #define RIPEMD160_AVX512_H
 
 #include <immintrin.h>
+#include <stdint.h>
 
-#include <cstdint>
+class RIPEMD160_AVX512 {
+ public:
+  RIPEMD160_AVX512();
+  ~RIPEMD160_AVX512();
+  void Init();
+  int Check512();
+  void Transform(uint64_t* input64, uint32_t* state, uint32_t rounds = 1);
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-// Full batch AVX-512: 16 blocks with 64 bits, 16 hashes with 20 bits
-void ripemd160_avx512_16blocks(const uint8_t* in[16], uint8_t* out[16]);
-
-// Batch 8x
-void ripemd160_avx512_8blocks(const uint8_t* in[8], uint8_t* out[8]);
-
-// Batch 1x
-void ripemd160_avx512_1block(const uint8_t* in, uint8_t* out);
-
-#ifdef __cplusplus
-}
-#endif
+ private:
+  __m512i* __attribute__((aligned(64))) w512;
+  uint32_t __attribute__((aligned(64))) _w[16][16];
+};
 
 #endif  // RIPEMD160_AVX512_H
